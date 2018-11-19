42c280d14a5325b9670dff23b88546968a4f246e
Jim <jim.ruan@perkinelmer.com>
2018-11-19 15:30:08 +0800

    Copyright info.

--- SpotfireTestTool/SpotfireTestTool/CustomAddIn.cs
+++ SpotfireTestTool/SpotfireTestTool/CustomAddIn.cs
@@ -1,8 +1,21 @@
-using System;
-using System.Windows.Forms;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomAddIn.cs" company="PerkinElmer Inc.">
+//   Copyright (c) 2013 PerkinElmer Inc.,
+//     940 Winter Street, Waltham, MA 02451.
+//     All rights reserved.
+//     This software is the confidential and proprietary information
+//     of PerkinElmer Inc. ("Confidential Information"). You shall not
+//     disclose such Confidential Information and may not use it in any way,
+//     absent an express written license agreement between you and PerkinElmer Inc.
+//     that authorizes such use.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-using Spotfire.Dxp.Application;
+#region
+
 using Spotfire.Dxp.Application.Extension;
+
+#endregion
 
 namespace Com.PerkinElmer.Service.SpotfireTestTool
 {
@@ -10,6 +23,5 @@ namespace Com.PerkinElmer.Service.SpotfireTestTool
     /// </summary>
     public sealed class CustomAddIn : AddIn
     {
-        // Override methods in this class to register your extensions.
     }
 }
