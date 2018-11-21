51fb8684eb5f937ca41b7570c6d4a4b13f046c96
Jim <jim.ruan@perkinelmer.com>
2018-11-21 16:19:35 +0800

    Add tool dialog.

---
+++ SpotfireTestTool/SpotfireTestTool/Views/ToolSettingsDialog.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ToolSettingsDialog.cs" company="PerkinElmer Inc.">
+//   Copyright (c) 2013 PerkinElmer Inc.,
+//     940 Winter Street, Waltham, MA 02451.
+//     All rights reserved.
+//     This software is the confidential and proprietary information
+//     of PerkinElmer Inc. ("Confidential Information"). You shall not
+//     disclose such Confidential Information and may not use it in any way,
+//     absent an express written license agreement between you and PerkinElmer Inc.
+//     that authorizes such use.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+#region
+
+using System.Windows.Forms;
+using Spotfire.Dxp.Application;
+
+#endregion
+
+namespace Com.PerkinElmer.Service.SpotfireTestTool.Views
+{
+    public partial class ToolSettingsDialog : Form
+    {
+        public ToolSettingsDialog(Document document)
+        {
+            InitializeComponent();
+        }
+    }
+}
